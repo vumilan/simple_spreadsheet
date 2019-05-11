7d55e7f4606fb0c2682300d4bdc4e1e1bf3206d2
vumilan <vumilan.long@gmail.com>
2019-05-11 21:37:04 +0200

    Add CLion files

---
+++ cmake-build-debug/CMakeFiles/simple_spreadsheet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_spreadsheet.dir/main.cpp.o"
+  "simple_spreadsheet.pdb"
+  "simple_spreadsheet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_spreadsheet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
